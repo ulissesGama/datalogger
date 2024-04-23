cf8324b8847433780475fc4430a5c19dec5c9436
ulissesGama <167483821+ulissesGama@users.noreply.github.com>
2024-04-23 20:12:48 -0300

    refatorização das funções, nova função do micro sd

--- datalog.ino
+++ datalog.ino
@@ -23,11 +23,14 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
-#define chipSelectPin 5 // Pino do ESP32 conectado ao chip select (CS) do módulo do cartão SD
+
+#define chipSelectPin 5  // Pino do ESP32 conectado ao chip select (CS) do módulo do cartão SD
 #define presFreDiaPin 36 // Pino do potenciômetro da pressão de fluido de freio dianteiro
 #define presFreTraPin 39 // Pino do potenciômetro da pressão de fluido de freio traseiro
 #define suspPosVolPin 25 // Pino do potenciômetro da posição do volante
-int presFreDia, presFreTra, suspPosVol;
+#define interval 80      // Intervalo desejado entre leituras em milissegundos
+#define dev
+
 Adafruit_MPU6050 mpu;
 RTC_DS3231 rtc;
 File dataFile;
@@ -38,34 +41,48 @@ sensors_event_t a, g, temp; // Variáveis globais para os dados do sensor
 float accelX, accelY, accelZ;
 float gyroX, gyroY, gyroZ;
 float tempC;
-unsigned long previousMillis = 0; // Variável para armazenar o tempo da última leitura
-const long interval = 80; // Intervalo desejado entre leituras em milissegundos
-int readingsCount = 0; // Contador de leituras por segundo
-int previousSecond = 0;           // Variável para armazenar o tempo anterior
-void setup(void)
+uint32_t previousMillis = 0; // Variável para armazenar o tempo da última leitura
+uint16_t readingsCount = 0;  // Contador de leituras por segundo
+uint32_t previousSecond = 0; // Variável para armazenar o tempo anterior
+
+/// Define variáveis globais para armazenar os dados da Susp
+uint32_t presFreDia = 0;
+uint32_t presFreTra = 0;
+uint32_t suspPosVol = 0;
+
+void componentes()
 {
-  // Inicializa a comunicação serial
-  Serial.begin(115200);
-  // Inicialização dos componentes
   if (!rtc.begin())
   {
+#ifdef dev
     Serial.println("Falha ao inicializar o RTC");
     delay(1000);
-    // esp_restart();
+    esp_restart();
+#endif
   }
   if (!mpu.begin(0x69))
   { // Endereço definido apenas com VCC no pino AD0 do MPU6050
+#ifdef dev
     Serial.println("Falha ao inicializar o MPU6050.");
-    // esp_restart();
+// esp_restart();
+#endif
   }
   if (!SD.begin(chipSelectPin))
   {
     Serial.println("Falha ao inicializar o cartão SD.");
     esp_restart();
   }
+}
+
+void configMPU()
+{
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G); //Define a faixa de medição do acelerômetro
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG); //Define a faixa de medição do giroscópio
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ); //Define a largura de banda do filtro do sensor
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);      //Define a faixa de medição do giroscópio
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);   //Define a largura de banda do filtro do sensor
+}
+
+String nomeArquivo()
+{
   // Criar uma string para armazenar o nome do arquivo
   String fileName = "/datalogger_";
   // Obter a data e hora atual do RTC
@@ -83,8 +100,12 @@ void setup(void)
   fileName += "_";
   fileName += String(now.second());
   fileName += ".txt";
-  // Abrir o arquivo com o nome criado para escrita
-  dataFile = SD.open(fileName, FILE_WRITE);
+  return fileName;
+}
+
+void inicializaArquivo()
+{
+  dataFile = SD.open(nomeArquivo(), FILE_WRITE); // Abrir o arquivo com o nome criado para escrita
   if (dataFile)
   {
     Serial.println("Arquivo aberto. Salvando dados...");
@@ -94,6 +115,10 @@ void setup(void)
     Serial.println("Erro ao abrir o arquivo.");
     esp_restart();
   }
+}
+
+void verificaRTC()
+{
   if (rtc.lostPower())
   {
     Serial.println("RTC perdeu energia. Defina a hora novamente.");
@@ -106,12 +131,16 @@ void setup(void)
   // Ou para atrasar 1 minuto:
   // DateTime now = rtc.now();
   // rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute() - 1, now.second()));
+}
+
+void calibraGiroscopio()
+{
   // Realiza a calibração do acelerômetro:
   // calibrateAccel();
   // Realiza a calibração do giroscópio:
   // calibrateGyro();
-  delay(10);
 }
+
 void RTC()
 {
   // Atualiza a variável global 'now' com a data e hora atuais
@@ -130,6 +159,7 @@ void RTC()
   Serial.print(now.second());
   Serial.print(" ");
 }
+
 void MPU()
 {
   sensors_event_t a, g, temp;
@@ -160,6 +190,7 @@ void MPU()
   Serial.print(temp.temperature);
   Serial.print(" ");
 }
+
 void Freios()
 {
   presFreDia = analogRead(presFreDiaPin); // Lê o valor do potenciômetro da pressão de fluido de freio dianteiro
@@ -169,12 +200,14 @@ void Freios()
   Serial.print(presFreTra);
   Serial.print(" ");
 }
+
 void Suspensao()
-{ 
+{
   suspPosVol = analogRead(suspPosVolPin); // Lê o valor do potenciômetro da posição do volante
   Serial.print(suspPosVol);
   Serial.print(" ");
 }
+
 void microSD()
 {
   if (dataFile)
@@ -219,25 +252,90 @@ void microSD()
     Serial.println("Erro ao escrever no arquivo.");
   }
 }
+
+void microSDNovo()
+{
+  if (dataFile)
+  {
+    String data = String(now.year());
+    String data = +";";
+    String data = data + String(now.month());
+    String data = +";";
+    String data = data + String(now.day());
+    String data = +";";
+    String data = data + String(now.hour());
+    String data = +";";
+    String data = data + String(now.minute());
+    String data = +";";
+    String data = data + String(now.second());
+    String data = +";";
+    String data = data + String(accelX);
+    String data = +";";
+    String data = data + String(accelY);
+    String data = +";";
+    String data = data + String(accelZ);
+    String data = +";";
+    String data = data + String(gyroX);
+    String data = +";";
+    String data = data + String(gyroY);
+    String data = +";";
+    String data = data + String(gyroZ);
+    String data = +";";
+    String data = data + String(tempC);
+    String data = +";";
+    String data = data + String(presFreDia);
+    String data = +";";
+    String data = data + String(presFreTra);
+    String data = +";";
+    String data = data + String(suspPosVol);
+    String data = +";";
+    String data = +"\n";              // Pula linha para proxima leitura
+    dataFile.print(data);
+    dataFile.flush(); // Força a gravação dos dados no arquivo
+  }
+  else
+  {
+    Serial.println("Erro ao escrever no arquivo.");
+  }
+}
+
+void setup()
+{
+
+#ifdef dev
+  Serial.begin(115200); // Inicializa a comunicação serial
+#endif
+  componentes(); // Inicialização dos componentes
+  configMPU();
+  inicializaArquivo();
+  verificaRTC();
+  calibraGiroscopio();
+  delay(10);
+}
+
 void loop()
 {
   DateTime now = rtc.now(); // Obtém a hora atual do RTC
-  // Verifica se mudou o segundo para reiniciar o contador de leituras
-  if (now.second() != previousSecond)
+
+  if (now.second() != previousSecond) // Verifica se mudou o segundo para reiniciar o contador de leituras
   {
     previousSecond = now.second();
     readingsCount = 0;
   }
-  // Verifica se ainda não atingiu 10 leituras no segundo atual
-  if (readingsCount < 10)
+
+  if (readingsCount < 10) // Verifica se ainda não atingiu 10 leituras no segundo atual
   {
-    unsigned long currentMillis = millis(); // Obtém o tempo atual em milissegundos if (currentMillis - previousMillis >= interval) { // Verifica se é hora de fazer a próxima leitura
-    previousMillis = currentMillis;         // Atualiza o tempo da última leitura
-    RTC();                                  // Atualiza 'now' com os dados do RTC
-    MPU();                                  // Atualiza 'a', 'g', 'temp' com os dados do sensor MPU6050 Freios(); // Atualiza dados referentes aos freios
-    Suspensao();                            // Atualiza dados referentes a suspensao
-    microSD();                              // Salva os dados no cartão SD
-    Serial.println();                       // Pula linha para próxima leitura
-    readingsCount++;                        // Incrementa o contador de leituras
+    unsigned long currentMillis = millis(); // Obtém o tempo atual em milissegundos
+    if (currentMillis - previousMillis >= interval)
+    {                                 // Verifica se é hora de fazer a próxima leitura
+      previousMillis = currentMillis; // Atualiza o tempo da última leitura
+      RTC();                          // Atualiza 'now' com os dados do RTC
+      MPU();                          // Atualiza 'a', 'g', 'temp' com os dados do sensor MPU6050
+      Freios();                       // Atualiza dados referentes aos freios
+      Suspensao();                    // Atualiza dados referentes a suspensao
+      microSD();                      // Salva os dados no cartão SD
+      Serial.println();               // Pula linha para próxima leitura
+      readingsCount++;                // Incrementa o contador de leituras
+    }
   }
 }
